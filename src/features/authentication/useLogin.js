d83b9d46d1e5df441479d68ec7f496d30ed530ec
rizal5516 <rizalqowi17@gmail.com>
2023-11-21 21:03:17 +0700

    authenticated-protected

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -2,10 +2,16 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login as loginApi } from '../../services/apiAuth';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
+import { useUser } from './useUser';
 
 export function useLogin() {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+
+  const { isAuthenticated } = useUser();
+  if (isAuthenticated) {
+    navigate("/dashboard");
+  };
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
